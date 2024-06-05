1bbfe44db79b0d0881efe1eaedf7ae540a317c8f
MINT <mint.2978@gmail.com>
2024-06-05 13:49:36 +0700

    feat: add show text input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingText, setGreetingText] = useState("Greeting Message");
+  const [greetingInput, setGreetingInput] = useState("");
+  console.log(greetingInput);
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingText}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={greetingInput}
+          onChange={(event) => {
+            setGreetingInput(event.target.value);
+          }}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button
+          onClick={() => {
+            setGreetingText(greetingInput);
+          }}
+        >
+          Update text
+        </button>
       </div>
     </div>
   );
